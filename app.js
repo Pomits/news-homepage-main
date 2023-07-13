d6fbdf63341e8cc5124aac59526d32c853003fd4
Pomits <127803387+Pomits@users.noreply.github.com>
2023-07-12 21:07:41 -0300

    uppload files

---
+++ app.js
@@ -0,0 +1,27 @@
+let btn = document.querySelector(".menu__logo");
+let list = document.querySelector(".lists");
+let body = document.querySelector(".max__container");
+let op = document.querySelectorAll(".list");
+
+function menu(){
+    btn.addEventListener("click", ()=>{
+        list.style.display = "flex";
+        body.style.display = "block";
+        setTimeout(()=>{list.style.transform = "translate(-100%)"}, 1);
+    })
+    
+    op.forEach((op)=>{
+        op.addEventListener("click", ()=>{
+            list.style.transform = "translate(100%)"
+            setTimeout(()=>{
+                list.style.display = "none";
+                body.style.display = "none";
+            }, 500);
+
+        })
+    })
+}
+
+if(window.screen.width <= 375){
+    menu();
+}
